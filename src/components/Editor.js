75b81ad284f8547e4ed5c083e0ad33a38976aad7
Mustafa Alsoffi <alsoffimustafa@gmail.com>
2020-07-10 14:11:33 +0800

    App has met all requirement

--- src/components/Editor.js
+++ src/components/Editor.js
@@ -2,19 +2,40 @@
 import React, { useState } from 'react'
 import Previewer from './Previewer';
 
+
 export default function Editor() {
-    
-    const [text , setText] = useState('');
+    const defaultText = `# Header 
+## Sub header
+[Link to FCC](https://www.freecodecamp.org/learn)
+\`\`\`
+/*a code block:*/
+.thick-green-border {
+    border-color: green;
+    border-width: 10px;
+    border-style: solid;
+    border-radius: 50%;
+  }
+\`\`\`
+Inline code: \`<span></span>\`
+- Item 1
+- Item 2
+
+> An unexamined life is not worth living.
+Socrates
+
+**And finally, an image:**![freeCodeCamp Logo](https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcSo6Gx6OrpizPFH8zCfDAZi810TPoNq4nbkyvnGSfr4L-SS4mmV&usqp=CAU)
+`;
+    const [text , setText] = useState(defaultText);
     const handleChange = (e) => {
-        setText(text + e.target.value) 
+        setText(e.target.value) 
     };
     return (
         <div  className='window'>
-         <div id='editor'>
+         <div id='editor-widnow'>
              <h3 className='title'>editor</h3>
           
-            <textarea onChange={handleChange} name='markdown-text' type='textarea'>
-                
+            <textarea  onChange={handleChange} name='markdown-text' type='textarea' id='editor'>
+            {defaultText}
             </textarea>
          </div>
 
--- src/components/Previewer.js
+++ src/components/Previewer.js
@@ -1,11 +1,22 @@
 
-import React from 'react'
+import React, { useEffect } from 'react'
 
 export default function Previewer(props) {
+    
+    var marked = require('marked');
+    marked.setOptions(
+        {
+          
+            breaks: true
+        }
+    );
+    useEffect(() => {
+        document.getElementById('preview').innerHTML = marked(props.text);
+      })
     return (
         <div id='previewer'>
             <h3 className='title'>previewer</h3>
-            <p>{props.text}</p>
+           <p id='preview'></p>
         </div>
     )
 }
